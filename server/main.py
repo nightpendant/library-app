5325e3885e6c5fe5618f791e6e8a6439f42c09b9
psionicflash <kerokerosene77@gmail.com>
2026-04-15 14:46:40 +0800

    Rudimentary integration, more proof of concept. Will begin proper architectural implementation soon.

--- server/main.py
+++ server/main.py
@@ -4,31 +4,46 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pprint import pprint
 
+from pydantic_core.core_schema import IncExDictOrElseSerSchema
+
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["*", "null"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 class Book:
-    def __init__(self, id=1, name="The Bible", borrower="Simone C. Torres", avg_rating=4.3):
+    def __init__(self, id=1, title="The Bible", author="Yahweh", genre="Religious Texts", borrower="Simone C. Torres", avg_rating=4.3):
         self.id = id
-        self.name = name
-        self.borrowers = borrower
+        self.title = title
+        self.borrower = borrower
+        self.author=author
+        self.genre = genre
         self.avg_rating = avg_rating
+
+class User:
+    def __init__(self, id=1, email="rnsagum@cbzrc.pshs.edu.ph", password="password"):
+        self.id = id 
+        self.email = email
+        self.password = password
 
 books = []
 books.append(Book())
-books.append(Book(2, "The Stranger", "Ridge V. Reodique", 4.5))
-books.append(Book(3, "Stranger Things", "", 4.5))
+books.append(Book(2, "The Stranger", "Albert Camus", "Absurdist Literature", "Ridge V. Reodique", 4.5))
+books.append(Book(3, "Perfect Strangers", "J.T. Geissinger", "Romance", "", 4.5))
+
+book_of_the_day = Book(67, "Atlas Shrugged", "Ayn Rand", "Misinformation", "Sky N. Cartano", 5.0)
 
 @app.post("/book")
 async def return_book(id):
+    if id == "day":
+        return book_of_the_day.__dict__
+
     id = int(id)
     pprint(id)
     found_book = books[id]
@@ -55,3 +70,4 @@ async def return_search_results(type, query):
 
     pprint(results)
     return results
+
